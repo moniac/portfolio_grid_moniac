301c3792626a24020e74a018e359473c50272a91
Mohammed Mulazada <thisismoniac@gmail.com>
2017-12-25 19:13:52 +0100

    service workers test

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let deferredPrompt
+
+if (!window.Promise) {
+	window.Promise = Promise
+}
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('/sw.js')
+		.then(() => {
+			// console.log('Service worker registered!')
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+
+window.addEventListener('beforeinstallprompt', (event) => {
+	// console.log('beforeinstallprompt fired')
+	event.preventDefault()
+	deferredPrompt = event
+	return false
+})
