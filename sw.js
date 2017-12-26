b926cf2c3b0d92712f61f1daa21fd07eaba29c49
Mohammed Mulazada <thisismoniac@gmail.com>
2017-12-26 01:28:42 +0100

    *service worker update

--- sw.js
+++ sw.js
@@ -1,59 +1,49 @@
-var CACHE_STATIC_NAME = 'static-v5';
-var CACHE_DYNAMIC_NAME = 'dynamic-v3';
+const CACHE_STATIC_NAME = 'static-v5'
+const CACHE_DYNAMIC_NAME = 'dynamic-v3'
 
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-      .then(function(cache) {
-        console.log('[Service Worker] Precaching App Shell');
-        cache.addAll([
-			'/',
-			'/index.html',
-			'/js/smoothscroll.js',
-			'/css/styles1.css',
-			'https://fonts.googleapis.com/css?family=Lato:400,900',
-			'/img/*'
-        ]);
-      })
-  )
-});
+self.addEventListener('install', (event) => {
+	console.log('[Service Worker] Installing Service Worker ...', event)
+	event.waitUntil(caches.open(CACHE_STATIC_NAME)
+		.then((cache) => {
+			console.log('[Service Worker] Precaching App Shell')
+			cache.addAll([
+				'/',
+				'/index.html',
+				'/js/smoothscroll.js',
+				'/css/styles1.css',
+				'https://fonts.googleapis.com/css?family=Lato:400,900',
+				'/img/*',
+			])
+		}) )
+})
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-  return self.clients.claim();
-});
+self.addEventListener('activate', (event) => {
+	console.log('[Service Worker] Activating Service Worker ....', event)
+	event.waitUntil(caches.keys()
+		.then(keyList => Promise.all(keyList.map((key) => {
+			if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+				console.log('[Service Worker] Removing old cache.', key)
+				return caches.delete(key)
+			}
+		}))) )
+	return self.clients.claim()
+})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_NAME)
-                .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function(err) {
+self.addEventListener('fetch', (event) => {
+	event.respondWith(caches.match(event.request)
+		.then((response) => {
+			if (response) {
+				return response
+			}
+			return fetch(event.request)
+				.then(res => caches.open(CACHE_DYNAMIC_NAME)
+					.then((cache) => {
+						cache.put(event.request.url, res.clone())
+						return res
+					}))
+				.catch((err) => {
 
-            });
-        }
-      })
-  );
-});
+				})
+
+		}) )
+})
