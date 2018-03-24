293836699a0daff871ad2623902c501d3c150756
Mohammed Mulazada <thisismoniac@gmail.com>
2018-03-24 20:58:49 +0100

    update sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_STATIC_NAME = 'static-v8'
-const CACHE_DYNAMIC_NAME = 'dynamic-v6'
+const CACHE_STATIC_NAME = 'static-v9'
+const CACHE_DYNAMIC_NAME = 'dynamic-v7'
 
 self.addEventListener('install', (event) => {
 	console.log('[Service Worker] Installing Service Worker ...', event)
